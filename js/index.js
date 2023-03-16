58cb77143cb887e5b6dbd573d5f64e1c52da136f
hasnat <themespassionh@gmail.com>
2023-03-16 23:10:06 +0600

    js file editing

--- js/index.js
+++ js/index.js
@@ -1,30 +1,3 @@
-// let text = "HELLO WORLD";
-// let letter = text.charAt(text.length - 1);
-// let letter1 = text.charAt(6);
-// let code = text.charCodeAt(text.length - 1);
-// let codePoint = text.codePointAt(2);
-
-// let text1 = "sea";
-// let text2 = " food";
-// let result = text1.concat(text2);
-// console.log(result);
-
-// let txt = "Hello world welcome to the universe.The rain in SPAIN stays mainly in the plain";
-
-// let res = txt.match(/lin/gi);
-
-// let rptext = "Mr Blue has a blue house and a blue car";
-// let rpResult = rptext.replace(/blue/g, "red");
-// console.log(rpResult);
-// let text = "Mr. Blue has a blue house";
-// let position = text.search(/blue/i);
-// console.log(position);
-// let text = "Hello world!";
-// let result = text.slice(1);
-// console.log(result);
-// let text = "How are you doing today?";
-// const myArray = text.split("").filter(item => item !== ' ');
-// console.log(myArray);
 
 let hour = document.getElementById("hour");
 let minute = document.getElementById("min");
@@ -35,7 +8,6 @@ let progress = document.getElementById("progress");
 
 var now = new Date();
 var hours = now.getHours() % 12 || 12;
-console.log(hours);
 
 let clockStart = () => {
     let date = new Date();
